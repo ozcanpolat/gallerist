e032f1ac187332cbc17ec8d47a595d975bd91c6d
ozcanpolat <ozcanp132@gmail.com>
2025-07-02 18:57:04 +0300

    exception message type add

--- src/main/java/com/ozcanpolat/gallerist/exception/MessageType.java
+++ src/main/java/com/ozcanpolat/gallerist/exception/MessageType.java
@@ -5,13 +5,16 @@ import lombok.Getter;
 @Getter
 public enum MessageType {
 
-    NO_RECORD_EXIST("1004" , "kayıt bulunamadı"),
-    TOKEN_IS_EXPIRED("1005" , "tokenın süresi bitmiştir"),
-    USERNAME_NOT_FOUND("1006" , "username bulunamadı"),
-    USERNAME_OR_PASSWORD_INVALID("1007" , "kullanıcı adı veya şifre hatalı"),
-    REFRESH_TOKEN_NOT_FOUND("1008" , "refresh token bulunamadı"),
-    REFRESH_TOKEN_IS_EXPIRED("1009" , "refresh tokenın süresi bitmiştir"),
-    GENERAL_EXCEPTION("9999" , "genel bir hata oluştu");
+    NO_RECORD_EXIST("1004" , "Kayıt bulunamadı"),
+    TOKEN_IS_EXPIRED("1005" , "Tokenın süresi bitmiştir"),
+    USERNAME_NOT_FOUND("1006" , "Username bulunamadı"),
+    USERNAME_OR_PASSWORD_INVALID("1007" , "Kullanıcı adı veya şifre hatalı"),
+    REFRESH_TOKEN_NOT_FOUND("1008" , "Refresh token bulunamadı"),
+    REFRESH_TOKEN_IS_EXPIRED("1009" , "Refresh tokenın süresi bitmiştir"),
+    CURRENY_RATES_IS_OCCURED("1010" , "Döviz kuru alınamadı"),
+    CUSTOMER_AMOUNT_IS_NOT_ENOUGH("1011" , "Müşterinin parası yeterli değildir"),
+    CAR_STATUS_IS_ALREADY_SALED("1012" , "Araba satılmış göründüğü için satılamaz"),
+    GENERAL_EXCEPTION("9999" , "Genel bir hata oluştu");
 
     private String code;
     private String message;
